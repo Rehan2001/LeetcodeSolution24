29026cd03d8650cc000176fd8479e2874f72ff96
Rehan Ahmad <83022732+Rehan2001@users.noreply.github.com>
2023-09-23 21:59:50 +0530

    Time: 0 ms (100.00%) | Memory: 6.6 MB (5.87%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.c
@@ -0,0 +1,14 @@
+
+
+int numIdenticalPairs(int* nums, int numsSize){
+    int ans = 0;
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i+1; j < numsSize; j++){
+            if(i != j && nums[i] == nums[j]){
+                ans += 1;
+            }
+        }
+    }
+    return ans;
+
+}
