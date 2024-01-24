494b092090cd31d61816d253f7570e4c56de1d0a
Rehan Ahmad <83022732+Rehan2001@users.noreply.github.com>
2024-01-24 17:36:22 +0530

    Time: 21 ms (79.27%) | Memory: 15.9 MB (28.30%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -2,37 +2,35 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
-        vector<vector<int>> ans;
-
         int n = nums.size();
-        for(int i = 0; i < n; i++)
-        {
-            if(i > 0 and nums[i] == nums[i - 1]) continue;
-            for(int j = i + 1; j < n; j++)
-            {
-                if(j > (i + 1) and nums[j] == nums[j - 1]) continue;
-                
-                int start = j + 1;
-                int end = n - 1;
+        vector<vector<int>> ans;
+        for(int i = 0; i <= n-4; i++){
+            // stop duplicate of i
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
 
-                while(start < end)
-                {
-                    long long sum = nums[i];
+            for(int j = i+1; j <= n-3; j++){
+                // stop duplicate of j
+                if(j > (i + 1) && nums[j] == nums[j - 1]) continue;
+
+                int k = j+1;
+                int e = n-1;
+                while(k < e){
+                    long long int sum = nums[i];
                     sum += nums[j];
-                    sum += nums[start];
-                    sum += nums[end];
-
-                    if(sum == target)
-                    {
-                        ans.push_back({nums[i] , nums[j] , nums[start] , nums[end]});
-                        int low = nums[start];
-                        int high = nums[end];
-                        start++,end--;
-                        while(start < end and nums[start] == low) start++;
-                        while(start < end and nums[end] == high) end--;
+                    sum += nums[k]; 
+                    sum += nums[e];
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[k],nums[e]});
+                        int low = nums[k];
+                        int high = nums[e];
+                        k++;
+                        e--;
+                        //stop duplicate of k & e
+                        while(nums[k] == low && k < e) k++;
+                        while(nums[e] == high && k < e) e--;
                     }
-                    else if(sum > target) end--;
-                    else start++;
+                    else if(sum > target) e--;
+                    else k++;
                 }
             }
         }
